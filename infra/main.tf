83cfe3f8f3db64e165ea308bbbd90a63aa517166
Ashwin Vats <av18011998@gmail.com>
2025-11-05 02:24:46 +0530

    Implement Terraform modules for VPC, IAM, and EKS with outputs and variables

---
+++ infra/main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+module "iam" {
+  source       = "./modules/iam"
+  cluster_name = var.cluster_name
+}
+
+module "eks" {
+  source = "./modules/eks"
+
+  cluster_name     = var.cluster_name
+  vpc_id           = module.vpc.vpc_id
+  private_subnets  = module.vpc.private_subnets
+  public_subnets   = module.vpc.public_subnets
+  cluster_role_arn = module.iam.cluster_role_arn
+  node_role_arn    = module.iam.node_role_arn
+}
