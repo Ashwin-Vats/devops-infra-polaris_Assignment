e86b1ab0c1702272224a6c309cf4181895a90381
Ashwin Vats <av18011998@gmail.com>
2025-11-05 22:23:57 +0530

    change in eks module

---
+++ infra/modules/eks/variables.tf
@@ -0,0 +1,23 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "cluster_role_arn" {
+  type = string
+}
+
+variable "node_role_arn" {
+  type = string
+}
